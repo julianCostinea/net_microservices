9066da7810262adc4bba8665ae83d5a1f23836d3
Emil Julian Costinea <ejc@arp-hansen.dk>
2023-11-08 14:54:17 +0100

    added actions to couponService

--- Mango.Web/Service/CouponService.cs
+++ Mango.Web/Service/CouponService.cs
@@ -33,21 +33,39 @@ public class CouponService : ICouponService
 
     public async Task<ResponseDto?> GetCouponByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await _baseService.SendAsync(new RequestDto()
+        {
+            ApiType = SD.ApiType.GET,
+            Url = SD.CouponAPIBase + "/api/coupon/" + id
+        });
     }
 
     public async Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
     {
-        throw new NotImplementedException();
+        return await _baseService.SendAsync(new RequestDto()
+        {
+            ApiType = SD.ApiType.POST,
+            Data = couponDto,
+            Url = SD.CouponAPIBase + "/api/coupon"
+        });
     }
 
     public async Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto)
     {
-        throw new NotImplementedException();
+        return await _baseService.SendAsync(new RequestDto()
+        {
+            ApiType = SD.ApiType.PUT,
+            Data = couponDto,
+            Url = SD.CouponAPIBase + "/api/coupon"
+        });
     }
 
     public async Task<ResponseDto?> DeleteCouponAsync(int id)
     {
-        throw new NotImplementedException();
+        return await _baseService.SendAsync(new RequestDto()
+        {
+            ApiType = SD.ApiType.DELETE,
+            Url = SD.CouponAPIBase + "/api/coupon/" + id
+        });
     }
 }
